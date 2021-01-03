a5e6eb99bf84063625dbff749cc57762b0c93e43
Nick Chaloult <npchaloult@gmail.com>
2021-01-03 00:32:10 -0500

    Add logic to add/overwrite artist tag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use id3::{Tag, Version};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut artist_tag = Tag::new();
+    artist_tag.set_artist("Me Smile");
+
+    artist_tag.write_to_path("song.mp3", Version::Id3v24)?;
+    Ok(())
 }
